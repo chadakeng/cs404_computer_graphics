bf892cdd14c687414ed7b5cc7e0727ecfd6392f8
chadakeng <chada.keng@gmail.com>
2024-10-04 22:55:49 +0700

    implemented all the other code starters.

--- Lab2/part1/lab2_4_1_cubic_bezier_curve.py
+++ Lab2/part1/lab2_4_1_cubic_bezier_curve.py
@@ -9,29 +9,30 @@ cv2.namedWindow(windowName)
 # Store points for the Bezier curve
 control_points = []
 
-def cubic_bezier_point(p0, p1, p2, t):
-    # Algorithm to draw a cubic Bezier curve 
-    pass
+def cubic_bezier_point(p0, p1, p2, p3, t):
+    return ((1 - t)**3 * p0 +
+            3 * (1 - t)**2 * t * p1 +
+            3 * (1 - t) * t**2 * p2 +
+            t**3 * p3)
 
 def draw_bezier_curve(control_points):
-    
-    num_curves = 0  # compute number of curves given the control_points
-    for nc in range(num_curves):
-        P0, P1, P2 = [] # Extract control points
+    P0, P1, P2, P3 = control_points
 
-        # Draw the Bezier curve
-        for t in np.linspace(0, 1, 1000):
-            x = cubic_bezier_point(P0[0], P1[0], P2[0], t)
-            y = cubic_bezier_point(P0[1], P1[1], P2[1], t)
-            cv2.circle(img, (x, y), 1, (0, 255, 0), -1)
-
+    # Draw the Bezier curve
+    for t in np.linspace(0, 1, 1000):
+        x = int(cubic_bezier_point(P0[0], P1[0], P2[0], P3[0], t))
+        y = int(cubic_bezier_point(P0[1], P1[1], P2[1], P3[1], t))
+        cv2.circle(img, (x, y), 1, (0, 255, 0), -1)
 
 def mouse_callback(event, x, y, flags, param):
+    global control_points
+
     if event == cv2.EVENT_LBUTTONDOWN:
         cv2.circle(img, (x, y), 3, (0, 0, 255), -1)
         control_points.append((x, y))
-        if len(control_points) >= 3:
+        if len(control_points) == 4:
             draw_bezier_curve(control_points)
+            control_points = []
 
 cv2.setMouseCallback(windowName, mouse_callback)
 
--- Lab2/part1/lab2_4_2_quadratic_bezier_curve.py
+++ Lab2/part1/lab2_4_2_quadratic_bezier_curve.py
@@ -9,29 +9,30 @@ cv2.namedWindow(windowName)
 # Store points for the Bezier curve
 control_points = []
 
-def quadratic_bezier_point(p0, p1, p2, p3, t):
-    # Algorithm to draw a quadratic Bezier curve 
-    pass
+def quadratic_bezier_point(p0, p1, p2, t):
+    return (1 - t)**2 * p0 + 2 * (1 - t) * t * p1 + t**2 * p2
 
 def draw_bezier_curve(control_points):
+    if len(control_points) == 3:
+        return 
     
-    num_curves = 0  # compute number of curves given the control_points
-    for nc in range(num_curves):
-        P0, P1, P2, P3 = [] # Extract control points
+    P0, P1, P2 = control_points[:3]  # Extract control points
 
-        # Draw the Bezier curve
-        for t in np.linspace(0, 1, 1000):
-            x = quadratic_bezier_point(P0[0], P1[0], P2[0], P3[0], t)
-            y = quadratic_bezier_point(P0[1], P1[1], P2[1], P3[0], t)
-            cv2.circle(img, (x, y), 1, (0, 255, 0), -1)
+    # Draw the Bezier curve
+    for t in np.linspace(0, 1, 1000):
+        x = int(quadratic_bezier_point(P0[0], P1[0], P2[0], t))
+        y = int(quadratic_bezier_point(P0[1], P1[1], P2[1], t))
+        cv2.circle(img, (x, y), 1, (0, 255, 0), -1)
 
 
 def mouse_callback(event, x, y, flags, param):
+    global control_points
     if event == cv2.EVENT_LBUTTONDOWN:
         cv2.circle(img, (x, y), 3, (0, 0, 255), -1)
         control_points.append((x, y))
         if len(control_points) >= 4:
             draw_bezier_curve(control_points)
+            control_points = []
 
 cv2.setMouseCallback(windowName, mouse_callback)
 
